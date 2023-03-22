d9739aefe3484dcc8dc45fdfb12cceb9840305f3
Chris McCarthy <mccacr64@gmail.com>
2023-03-22 14:25:51 -0400

    changed connectDB and app.listen

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ require("dotenv").config({ path: "./config/.env" });
 require("./config/passport")(passport);
 
 //Connect To Database
-connectDB();
+// connectDB();
 
 //Using EJS for views
 app.set("view engine", "ejs");
@@ -65,6 +65,8 @@ app.use("/games", gamesRoutes);
 
 
 //Server Running
-app.listen(process.env.PORT, () => {
-  console.log("Server is running, you better catch it!");
+connectDB().then(() => {
+  app.listen(process.env.PORT, () => {
+    console.log("Server is running, you better catch it!");
+  });
 });
